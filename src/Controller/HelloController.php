d0027019e815eee77229b8eeabd5e1d28ac706c9
HUGUET Zachary <huguzac@ndlp.fr>
2021-10-19 12:46:18 +0200

    first commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HelloController{
+    /**
+     * @Route("/hello")
+     */
+    public function hello(){
+        return new Response("Hello World! ");
+    }
+
+}

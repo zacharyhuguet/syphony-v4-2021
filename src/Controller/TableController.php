9537dd63cecb922e4b9418a20f4edc556149d759
HUGUET Zachary <huguzac@ndlp.fr>
2021-10-19 14:35:41 +0200

    2

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/table", name="table")
@@ -14,12 +15,14 @@ class TableController extends AbstractController
     /**
      * @Route("/print/{n}/{i}", name="table_print")
      */
-    public function index(int $n, int $i): Response
+    public function index(int $n, int $i, Request $request): Response
     {
+        $color = $request->get('c');
         return $this->render('table/index.html.twig', [
             'controller_name' => 'TableController',
             'n'=> $n,
             'i'=> $i,
+            'color'=>$color
         ]);
     }
 }

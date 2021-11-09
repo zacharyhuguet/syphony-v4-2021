a77b84e51c60c440c6622cc6d49814505aad25b7
HUGUET Zachary <huguzac@ndlp.fr>
2021-11-09 12:20:47 +0100

    2

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Table;
+use App\Form\TableChoiceType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +16,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TableController extends AbstractController
 {
     /**
-     * @Route("/print/{n}/{i}", name="table_print")
+     * @Route("/select",name="table_select")
+     */
+    public function select(Request $request)
+    {
+        $form = $this->createForm(TableChoiceType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            $n = $data['table_number'];
+            $m = $data['lines_count'];
+            $color = $data['color'];
+
+            $table = new Table($n);
+            $calculations = $table->calcMultiply($m);
+
+            dump($n);
+            $response = $this->render('table/index.html.twig', [
+                'controller_name' => 'TableController',
+                'n' => $n,
+                'calculations'  => $calculations,
+                'color' => $color,
+            ]);
+
+        } else {
+            dump("NOT isSubmitted");
+            $response = $this->render('table/vue.html.twig', [
+                'Formulaire' => $form->createView(),
+            ]);}
+
+        return $response;
+    }
+
+    /**
+     * @Route("/print/{n}/{i}/{color}", name="table_print")
      */
     public function index(int $n, int $i, Request $request): Response
     {
         $color = $request->get('c');
         return $this->render('table/index.html.twig', [
             'controller_name' => 'TableController',
-            'n'=> $n,
-            'i'=> $i,
-            'color'=>$color
+            'n' => $n,
+            'i' => $i,
+            'color' => $color,
         ]);
     }
 }
---
+++ src/Form/TableChoiceType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TableChoiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('table_number', ChoiceType::class,
+            ['choices' => [
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 7,
+                '8' => 8,
+                '9' => 9,
+            ],
+        ]);
+
+        $builder->add('lines_count', ChoiceType::class,
+                ['choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                ],
+            ]);
+
+            $builder->add('color', ColorType::class);
+
+            $builder->add('envoyer', SubmitType::class,
+                [
+                    'attr' => ['class' => 'save'],
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
